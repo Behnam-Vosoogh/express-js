998027677c01068fd3108d276c30d06abe559eca
Behnam <behnam.vosoogh@outlook.com>
2025-02-09 12:17:08 -0500

    404 page added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const adminRoutes = require("./routes/admin");
@@ -9,7 +10,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  // res.status(404).send("<h1>Page not found</h1>");
 });
 // app.use("/", (req, res, next) => {
 //   console.log("This always runs!");
