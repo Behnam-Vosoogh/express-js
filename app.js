be660ffbd65046b2694489fcdc9cb1a5ef24d0eb
Behnam <behnam.vosoogh@outlook.com>
2025-02-19 12:13:07 -0500

    80-sharing data ,Requests & users

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-const adminRoutes = require("./routes/admin");
+const AdminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/admin", adminRoutes);
+app.use("/admin", AdminData.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
 const path = require("path");
 const express = require("express");
 const rootDir = require("../util/path");
+const AdminData = require("./admin");
 
 const router = express.Router();
 router.get("/", (req, res, next) => {
+  console.log("shop.js", AdminData.products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
   // res.sendFile("shop.html", { root: "views" });
 });
