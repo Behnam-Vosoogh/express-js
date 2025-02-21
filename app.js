226945003cbf331384934ff65b38ec9157ad24fe
Behnam <behnam.vosoogh@outlook.com>
2025-02-21 16:32:47 -0500

    82-pug implemented

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
+app.set("view engine", "pug");
+app.set("views", "views");
 const AdminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,8 @@ const AdminData = require("./admin");
 const router = express.Router();
 router.get("/", (req, res, next) => {
   console.log("shop.js", AdminData.products);
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  res.render("shop");
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
   // res.sendFile("shop.html", { root: "views" });
 });
 module.exports = router;
